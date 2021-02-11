96b8dd84557340fa8bfdb2eb564a95cc24ed66dd
João Vieira <feijoca96@hotmail.com>
2021-02-11 12:12:45 +0000

    Changes Startup

--- UP Mobile/Startup.cs
+++ UP Mobile/Startup.cs
@@ -27,6 +27,9 @@ namespace UP_Mobile
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UPMobileContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("UPMobileContext")));
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
