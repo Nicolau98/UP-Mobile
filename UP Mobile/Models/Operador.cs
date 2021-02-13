1d06733d61ba2dd573a4c680b4ba11f38e861b77
Nicolau Barranha <nicolaubarranha999@hotmail.com>
2021-02-13 15:38:09 +0000

    Layout view operador

--- UP Mobile/Models/Operador.cs
+++ UP Mobile/Models/Operador.cs
@@ -17,22 +17,31 @@ namespace UP_Mobile.Models
 
         [Key]
         [Column("Id_Operador")]
+        [Display(Name = "Nº Operador")]
         public int IdOperador { get; set; }
+
         [Required]
         [StringLength(10)]
         public string Nome { get; set; }
+
         [Column("Data_Nascimento", TypeName = "date")]
+        [Display(Name = "Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
+
         [Required]
         [StringLength(50)]
         public string Morada { get; set; }
+
         public int Contacto { get; set; }
+
         [Required]
         [StringLength(20)]
         public string Email { get; set; }
+
         [Required]
         [Column("Local_Trabalho")]
         [StringLength(50)]
+        [Display(Name = "Morada do local de trabalho")]
         public string LocalTrabalho { get; set; }
 
         [InverseProperty("IdOperadorNavigation")]
