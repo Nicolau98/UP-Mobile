b232611b633e2dcf7f7b2213c545a2cecff0196e
Mariana <mariana.sofia.monteiro.santos@gmail.com>
2021-02-14 01:51:19 +0000

    template principal

--- UP Mobile/Controllers/HomeController.cs
+++ UP Mobile/Controllers/HomeController.cs
@@ -23,10 +23,16 @@ namespace UP_Mobile.Controllers
             return View();
         }
 
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
