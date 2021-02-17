6819ddfafe6a0f7bd3d5031c260fb223fc471fe2
Nicolau Barranha <nicolaubarranha999@hotmail.com>
2021-02-17 10:01:54 +0000

    Authorize Models Name Check ativo ou não

--- UP Mobile/Models/Administrador.cs
+++ UP Mobile/Models/Administrador.cs
@@ -12,24 +12,34 @@ namespace UP_Mobile.Models
     {
         [Key]
         [Column("Id_Administrador")]
+        [Display( Name = "Nº Administrador")]
         public int IdAdministrador { get; set; }
         [Required]
         [StringLength(50)]
         public string Nome { get; set; }
+
         [Column("Data_Nascimento", TypeName = "date")]
+        [Display(Name = "Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
+
         [Required]
         [StringLength(50)]
         public string Morada { get; set; }
+
         public int Contacto { get; set; }
+
         [Required]
         [StringLength(20)]
         public string Email { get; set; }
+
         [Required]
         [Column("Local_Trabalho")]
+        [Display(Name = "Morada do local de trabalho")]
         [StringLength(50)]
         public string LocalTrabalho { get; set; }
+
         [Column("Administrador_ativo")]
+        [Display(Name = "Administrador ativo")]
         public bool AdministradorAtivo { get; set; }
     }
 }
