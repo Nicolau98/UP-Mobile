9bd1556e80b5df9facd504845055520aa132708b
Nicolau Barranha <nicolaubarranha999@hotmail.com>
2021-02-24 09:52:58 +0000

    Correção de uma parcela de codigo

--- UP Mobile/Data/SeedData.cs
+++ UP Mobile/Data/SeedData.cs
@@ -55,7 +55,7 @@ namespace UP_Mobile.Data
 
         private static void InsereUtilizadoresFicticios(UPMobileContext context)
         {
-            if (context.Role.Any()) return;
+            if (context.Utilizador.Any()) return;
 
 
             Role rolecliente = context.Role.FirstOrDefault(r => r.Nome == "Cliente");
@@ -76,11 +76,14 @@ namespace UP_Mobile.Data
                         IdRoleNavigation = rolecliente
 
                     },
-            });
-    
 
-        //private static void InsereClientesFicticias(UPMobileContext context)
-        //{
+            });
+
+            context.SaveChanges();
+
+
+            //private static void InsereClientesFicticias(UPMobileContext context)
+            //{
             //    if (context.Cliente.Any()) return;
 
             //    //InsereClientesFicticiasParaTestar(context);
