c0286c04ff89fdc44dbc0f67c7dc8645ed7c9e11
Luís Bernardo <luishdbernardo@hotmail.com>
2021-02-26 11:23:48 +0000

    Algumas correções

--- UP Mobile/Startup.cs
+++ UP Mobile/Startup.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using UP_Mobile.Data;
 
 namespace UP_Mobile
