78e984541b0a2e7c1076bacb641db6883a866fdc
João Vieira <feijoca96@hotmail.com>
2021-02-26 15:22:53 +0000

    Display name em todos os Models Colocado carateres em contactos

--- UP Mobile/Models/PacoteComercial.cs
+++ UP Mobile/Models/PacoteComercial.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 #nullable disable
 
@@ -17,28 +18,44 @@ namespace UP_Mobile.Models
 
         [Key]
         [Column("Id_Pacote")]
+        [Display(Name = "Nº Pacote")]
         public int IdPacote { get; set; }
         [Required]
         [StringLength(50)]
+
         public string Nome { get; set; }
         [StringLength(500)]
+
         public string Descricao { get; set; }
+
         [Column("Data_Inicio_Comercializacao", TypeName = "date")]
+        [Display(Name = "Data de início da comercialização")]
         public DateTime DataInicioComercializacao { get; set; }
+
         [Column("Data_Fim_Comercializacao", TypeName = "date")]
+        [Display(Name = "Data de fim da comercialização")]
         public DateTime? DataFimComercializacao { get; set; }
+
         [Column("Preco_base", TypeName = "decimal(4, 2)")]
+        [Display(Name = "Preço base")]
         public decimal PrecoBase { get; set; }
+
         [Column("Periodo_Fidelizacao")]
+        [Display(Name = "Periodo Fidelização")]
         public int PeriodoFidelizacao { get; set; }
+
         public bool Ativo { get; set; }
         [StringLength(50)]
+
         public string Internet { get; set; }
         [StringLength(50)]
+
         public string Voz { get; set; }
         [StringLength(50)]
+
         public string Tv { get; set; }
         [StringLength(50)]
+
         public string Movel { get; set; }
 
         [InverseProperty("IdPacoteNavigation")]
