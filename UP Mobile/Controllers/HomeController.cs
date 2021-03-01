0186969950b56981062310a15fb085b3c0023269
Mariana <mariana.sofia.monteiro.santos@gmail.com>
2021-03-01 02:10:39 +0000

    continuação alteraçoes

--- UP Mobile/Controllers/HomeController.cs
+++ UP Mobile/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace UP_Mobile.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult UP()
         {
             return View();
         }
