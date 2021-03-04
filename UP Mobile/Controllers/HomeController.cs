5468be82160546c93a382c818238c820acd123cd
Nicolau Barranha <nicolaubarranha999@hotmail.com>
2021-03-04 11:27:54 +0000

    Continuação do modal já a funcionar

--- UP Mobile/Controllers/HomeController.cs
+++ UP Mobile/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace UP_Mobile.Controllers
             return View();
         }
 
+        public IActionResult teste()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
