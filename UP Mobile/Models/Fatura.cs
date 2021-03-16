7ef05db91c4175dcf6fb58cb571e62d54d740d36
Luís Bernardo <luishdbernardo@hotmail.com>
2021-03-16 15:23:02 +0000

    Alterações na tabela Fatura

--- UP Mobile/Models/Fatura.cs
+++ UP Mobile/Models/Fatura.cs
@@ -13,25 +13,21 @@ namespace UP_Mobile.Models
         [Key]
         [Column("Id_Fatura")]
         public int IdFatura { get; set; }
-
         [Column("Id_Contrato")]
         public int IdContrato { get; set; }
-
         [Column(TypeName = "datetime")]
         public DateTime Data { get; set; }
-
         [Column("Data_Limite_Pagamento", TypeName = "date")]
         public DateTime DataLimitePagamento { get; set; }
-
         [Required]
         [StringLength(500)]
         public string Descricao { get; set; }
-
         [Column("Preco_Total", TypeName = "decimal(5, 2)")]
         public decimal PrecoTotal { get; set; }
-
         [Column("Id_Metodo_Pagamento")]
         public int IdMetodoPagamento { get; set; }
+        [Column("Data_Pagamento", TypeName = "datetime")]
+        public DateTime DataPagamento { get; set; }
 
         [ForeignKey(nameof(IdContrato))]
         [InverseProperty(nameof(Contrato.Fatura))]
