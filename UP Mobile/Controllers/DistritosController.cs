ecc76fa393dc8b6fc24a98b2a507f4adce133d30
João Vieira <feijoca96@hotmail.com>
2021-03-16 12:10:38 +0000

    Scafolding Distrito, Metodo e Faturas

---
+++ UP Mobile/Controllers/DistritosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UP_Mobile.Data;
+using UP_Mobile.Models;
+
+namespace UP_Mobile.Controllers
+{
+    public class DistritosController : Controller
+    {
+        private readonly UPMobileContext _context;
+
+        public DistritosController(UPMobileContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Distritos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Distrito.ToListAsync());
+        }
+
+        // GET: Distritos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var distrito = await _context.Distrito
+                .FirstOrDefaultAsync(m => m.IdDistrito == id);
+            if (distrito == null)
+            {
+                return NotFound();
+            }
+
+            return View(distrito);
+        }
+
+        // GET: Distritos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Distritos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdDistrito,Nome")] Distrito distrito)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(distrito);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(distrito);
+        }
+
+        // GET: Distritos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var distrito = await _context.Distrito.FindAsync(id);
+            if (distrito == null)
+            {
+                return NotFound();
+            }
+            return View(distrito);
+        }
+
+        // POST: Distritos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdDistrito,Nome")] Distrito distrito)
+        {
+            if (id != distrito.IdDistrito)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(distrito);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DistritoExists(distrito.IdDistrito))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(distrito);
+        }
+
+        // GET: Distritos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var distrito = await _context.Distrito
+                .FirstOrDefaultAsync(m => m.IdDistrito == id);
+            if (distrito == null)
+            {
+                return NotFound();
+            }
+
+            return View(distrito);
+        }
+
+        // POST: Distritos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var distrito = await _context.Distrito.FindAsync(id);
+            _context.Distrito.Remove(distrito);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DistritoExists(int id)
+        {
+            return _context.Distrito.Any(e => e.IdDistrito == id);
+        }
+    }
+}
