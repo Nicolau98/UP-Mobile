4f9ba8dd049211059409e93e9258c7cd3c644fe7
Nuno Aniceto <anicetonuno74@gmail.com>
2021-03-30 00:40:28 +0100

    notificação do numero de reclamações em aberto

--- UP Mobile/Controllers/HomeController.cs
+++ UP Mobile/Controllers/HomeController.cs
@@ -3,7 +3,11 @@ using AspNetCoreHero.ToastNotification;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using UP_Mobile.Models;
+using UP_Mobile.Data;
 using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace UP_Mobile.Controllers
 {
@@ -11,16 +15,35 @@ namespace UP_Mobile.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly INotyfService _notyf;
-        public HomeController(ILogger<HomeController> logger, INotyfService notyf)
+        private readonly UPMobileContext _context;
+        public HomeController(ILogger<HomeController> logger, INotyfService notyf, UPMobileContext context)
         {
             _logger = logger;
             _notyf = notyf;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            _notyf.Custom("Custom Notification - closes in 20 seconds.", 20, "#4fd6d9", "fa fa-home");
-            return View();           
+
+            if (User.IsInRole("Operador"))
+            {
+                var operador = await _context.Utilizador.SingleOrDefaultAsync(o => o.Email == User.Identity.Name);
+                Estado estadoaberto = _context.Estado.FirstOrDefault(e => e.Nome == "Em Aberto");
+
+                var distritooperador = operador.IdDistrito;
+                
+
+                var totalemaberto = _context.Reclamacao.Where(c => (c.IdEstadoNavigation == estadoaberto)&& (c.IdClienteNavigation.IdDistrito == distritooperador))
+                    .Include(c => c.IdEstadoNavigation)
+                    .Include(c => c.IdOperadorNavigation)
+                    .Include(c => c.IdClienteNavigation)
+                    .Count();
+
+                _notyf.Custom("Existem " + totalemaberto + " Reclamações em aberto no seu Distrito", 20, "#4fd6d9", "fa fa-home");
+                
+            }
+            return View();
         }
 
         public IActionResult Main()
